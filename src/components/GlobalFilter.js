bc0e91795658dc26266628de45e4aba37acb1530
nvkex <scrbwrm@gmail.com>
2020-07-30 18:35:42 +0530

    components

---
+++ src/components/GlobalFilter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useAsyncDebounce } from 'react-table';
+
+// Global filter for all columns
+export default function GlobalFilter({globalFilter, setGlobalFilter }) {
+
+    const [value, setValue] = useState(globalFilter);
+    const onInputChange = useAsyncDebounce(value => {
+      setGlobalFilter(value || undefined)
+    }, 200);
+  
+    return (
+      <span>
+        <input
+          value={value || ""}
+          onChange={e => {
+            setValue(e.target.value);
+            onInputChange(e.target.value);
+          }}
+          placeholder="Search..."
+          style={{height:'5vh',width: '50%', margin:'0 25%'}}
+        />
+      </span>
+    )
+
+}
